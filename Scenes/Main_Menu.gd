31607c7856defd9f5d5167c85ce11f9eb873f13e
mdue1337 <martin@familiendue.dk>
2023-10-04 13:23:08 +0200

    #3 Done

--- Scenes/Main_Menu.gd
+++ Scenes/Main_Menu.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 func _on_StartLevel_pressed():
-	pass # Replace with function body.
+	get_tree().change_scene("res://Scenes/Main_Menu.tscn")
 
 func _on_Settings_pressed():
-	pass # Replace with function body.
+	get_tree().change_scene("res://Scenes/Rules.tscn")
