2640158be993c9f5a641e03bf369f00a5b47ca23
mdue1337 <martin@familiendue.dk>
2023-10-09 13:22:03 +0200

    #5 Done, #6 Globalscript

---
+++ Other/GS.gd
@@ -0,0 +1,4 @@
+extends Node
+
+var health = 100
+var chestCounter = 0
--- Scenes/Main_Menu.gd
+++ Scenes/Main_Menu.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 func _on_StartLevel_pressed():
-	get_tree().change_scene("res://Scenes/Main_Menu.tscn")
+	get_tree().change_scene("res://Scenes/Game.tscn")
 
 func _on_Settings_pressed():
 	get_tree().change_scene("res://Scenes/Rules.tscn")
