f36c32182342b69be6551f2dcb17055037a195b4
Aayan0909 <Aayanminhas0909@gmail.com>
2023-10-10 13:09:45 +0200

    Det virker nu

--- Models/Rock.gd
+++ Models/Rock.gd
@@ -6,3 +6,4 @@ func _on_Rock_body_entered(body):
 		body.take_damage()
 		print("damage")
 		Gs.emit_signal("updateHealth", Gs.health)
+		print("New health" + str(Gs.health))
--- Other/ChestCounter.gd
+++ Other/ChestCounter.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 func _ready():
+	Gs.chestCounter = 0
 	_on_chest_count_changed(Gs.chestCounter)
 	Gs.connect("updateCounter", self, "_on_chest_count_changed")
 
