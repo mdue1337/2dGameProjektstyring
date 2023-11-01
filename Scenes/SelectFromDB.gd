726fb938042244a435e45f6e1b0a7ccfedbaa5e2
Aayan0909 <Aayanminhas0909@gmail.com>
2023-11-01 15:40:43 +0100

    Revert "Merge branch 'main' of https://github.com/mdue1337/2dGameProjektstyring"
    This reverts commit 70f204e841193ffe84ac05ac17708bd61d6aeceb, reversing
    changes made to 25287c6184fd9d2fe1bba8cc0629fe145f9add8d.

--- Scenes/SelectFromDB.gd
+++ Scenes/SelectFromDB.gd
@@ -8,52 +8,38 @@ var port = 5432 #manden i videoen havde de tsom en int men det tror jeg ikke vir
 var databaseConnection = "xubugyyg"
 
 func _ready():
-	database.connect("connection_established",self, "VerifyLogin")
+	database.connect("connection_established",self, "selectFromDB")
 	database.connect("connection_error",self,"error")
 	database.connect("connection_closed",self,"closedConnection")
 	
 	database.connect_to_host("postgresql://%s:%s@%s:%d/%s" % [user, password, host, port, databaseConnection])
+		
 	pass
 
-func selectUserFromDB():
+func insertIntoDB(id,name,score):
 	print("running select query")
 	
-	var data = database.execute("SELECT * FROM public.\"Users\";")
+	var data = database.execute("""
+	BEGIN;
+	INSERT INTO leaderboard (id,name,score) values (%s, '%s', %s);
+	commit;
+	""" % [id,name,score])
 	
-	for i in range(data.size()):
-		for d in data[i].data_row:
-			print(str(d))
-	
+	for d in data[1].data_row:
+		print(d)
 	database.close()
 
-func insertDBUser():
-	print("Insert")
-	var fullname = Gs.fullname
-	var email = Gs.email
-	var password = Gs.password
+func selectFromDB():
+	print("running select query")
 	
-	var query = "BEGIN;\n"
-	query += "INSERT INTO public.\"Users\" (fullname, email, passwordtext) VALUES ('" + fullname + "', '" + email + "', '" + password + "');\n"
-	query += "COMMIT;"
+	var data = database.execute("""
+	BEGIN;
+	SELECT * FROM leaderboard;
+	""")
 	
-	database.execute(query)
+	for d in data:
+		print(d)
 	database.close()
-
-func VerifyLogin():
-	var email = Gs.loginEmail
-	var pw = Gs.loginPw
-	
-	var query = "SELECT COUNT(*) FROM public.\"Users\" WHERE email = '" + str(email) + "' AND passwordtext = '" + str(pw) + "'"
-	
-	var data = database.execute(query)
-	
-	if data[0].data_row[0][0] == 1:
-		print("login win")
-		Gs.loginsucess = true
-	else:
-		print("login fail")
-		Gs.loginsucess = false
-	
 
 func _process(delta):
 	database.poll()
